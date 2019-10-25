83e0a3c1282661ea5c4ad6504d9f3883e00cbd5d
ghost-fullstack <ghost.fullstack@gmail.com>
2019-10-25 21:51:31 +0300

    server side:   permission route

--- server/validator.js
+++ server/validator.js
@@ -5,11 +5,14 @@ const ajv = new Ajv({ schemaId: 'auto', allErrors: true });
 ajv.addSchema(readJson('schema', 'name.schema.json'));
 
 const newUserSchema = readJson('schema', 'new-user.schema.json');
-const newRolePermissionSchema = readJson('schema', 'new-role-permission.schema.json');
+const newRoleSchema = readJson('schema', 'new-role.schema.json');
+const newPermissionSchema = readJson('schema', 'new-permission.schema.json');
 const newUser = ajv.compile(newUserSchema);
-const newRolePermission = ajv.compile(newRolePermissionSchema);
+const newRole = ajv.compile(newRoleSchema);
+const newPermission = ajv.compile(newPermissionSchema);
 
 module.exports = {
   newUser,
-  newRolePermission
+  newRole,
+  newPermission
 };
