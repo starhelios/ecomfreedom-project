56d6fc67299459b9d12156ce1786b49b139fba3c
starhelios <starhelios94@gmail.com>
2019-10-25 18:09:03 +0300

    server side:  added role field to new user model

--- server/db.js
+++ server/db.js
@@ -8,9 +8,10 @@ mongoose.connect(config.get('db:url'), { useNewUrlParser: true });
 const USER = new mongoose.Schema({
   username: { type: String, unique: true },
   hash: String,
-  email: String,
-  firstname: String,
-  lastname: String
+  email: { type: String, index: true },
+  firstname: { type: String, index: true },
+  lastname: { type: String, index: true },
+  roles: { type: [String], index: true }
 });
 
 USER.statics.create = async ({
@@ -18,11 +19,12 @@ USER.statics.create = async ({
   password,
   email,
   firstname,
-  lastname
+  lastname,
+  roles
 }) => {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
-  const user = new User({ username, hash, email, firstname, lastname });
+  const user = new User({ username, hash, email, firstname, lastname, roles });
   await user.save();
   return user;
 };
