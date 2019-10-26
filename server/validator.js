0ac2f5ca0c7b51835223ea2160ef52d198460d18
bulat <bulat.i.safin@gmail.com>
2019-10-26 16:09:40 +0300

    server side:   roles one permission adding

--- server/validator.js
+++ server/validator.js
@@ -9,15 +9,18 @@ const id = readJson('schema', 'id-request.schema.json');
 const newUserSchema = readJson('schema', 'new-user.schema.json');
 const newRoleSchema = readJson('schema', 'new-role.schema.json');
 const newPermissionSchema = readJson('schema', 'new-permission.schema.json');
+const assignPermissionSchema = readJson('schema', 'assign-permission.schema.json');
 
 const mongoId = ajv.compile(id);
 const newUser = ajv.compile(newUserSchema);
 const newRole = ajv.compile(newRoleSchema);
 const newPermission = ajv.compile(newPermissionSchema);
+const assignPermission = ajv.compile(assignPermissionSchema);
 
 module.exports = {
   newUser,
   newRole,
   newPermission,
-  mongoId
+  mongoId,
+  assignPermission
 };
