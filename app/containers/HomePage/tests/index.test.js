c03c6e2905d2f29f7a3d5c2c366a5e76db4eba39
starhelios <starhelios94@gmail.com>
2019-11-04 01:19:28 +0300

    Fix tests

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import { IntlProvider } from 'react-intl';
 
 import HomePage from '../index';
 
 describe('<HomePage />', () => {
   it('should render and match the snapshot', () => {
     const {
-      container: { firstChild },
-    } = render(
-      <IntlProvider locale="en">
-        <HomePage />
-      </IntlProvider>,
-    );
+      container: { firstChild }
+    } = render(<HomePage />);
     expect(firstChild).toMatchSnapshot();
   });
 });
