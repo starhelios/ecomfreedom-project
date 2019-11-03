1f0ca727d2c51295014cc69683e767e00771164f
chesnok9 <pavel.chesnovsky@gmail.com>
2019-11-04 01:35:45 +0300

    Fix tests

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { render } from 'react-testing-library';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureMockStore from 'redux-mock-store';
 
+import history from 'utils/history';
 import HomePage from '../index';
+
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<HomePage />', () => {
   it('should render and match the snapshot', () => {
     const {
       container: { firstChild }
-    } = render(<HomePage />);
+    } = render(
+      <Provider store={store}>
+        <BrowserRouter history={history}>
+          <HomePage />
+        </BrowserRouter>
+      </Provider>
+    );
     expect(firstChild).toMatchSnapshot();
   });
 });
