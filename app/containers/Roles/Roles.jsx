d818762f6d7d6787990050cc86cfbf268235e656
ghost-fullstack <ghost.fullstack@gmail.com>
2019-11-05 00:29:04 +0300

    Users, Roles, Permissions.

---
+++ app/containers/Roles/Roles.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// core components
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Table from "components/Table/Table.jsx";
+import Card from "components/Card/Card.jsx";
+// import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+function TableList(props) {
+  const { classes } = props;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Roles
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardBody>
+              <Table
+                tableHeaderColor="info"
+                tableHead={["ID", "Name", "Country", "City", "Salary"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
+                  [
+                    "4",
+                    "Philip Chaney",
+                    "$38,735",
+                    "Korea, South",
+                    "Overland Park"
+                  ],
+                  [
+                    "5",
+                    "Doris Greene",
+                    "$63,542",
+                    "Malawi",
+                    "Feldkirchen in Kärnten"
+                  ],
+                  ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
+                ]}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </>
+  );
+}
+
+export default withStyles(styles)(TableList);
