aa5f6c288b5ce74c35f47c0710d971d579662d09
starhelios <starhelios94@gmail.com>
2019-11-05 18:48:24 +0300

    Revert "Update backend configs"
    This reverts commit c56b1b59

--- server/config.js
+++ server/config.js
@@ -3,16 +3,16 @@ const ajv = new Ajv({ schemaId: 'auto', allErrors: true });
 
 const ENV = 'NODE_ENV';
 const nconf = require('nconf');
-const { readJson } = require('./file-util');
+const { readJson, BASE_PATH } = require('./file-util');
+
 nconf
-  .use('memory')
   .argv()
   .env()
   .required([ENV]);
 
 const env = nconf.get(ENV);
-// const data = env === 'production' ? prod : dev;
-const data = require(`../config/${env}.js`);
+
+const data = readJson('config', `${env}.json`);
 ajv.addSchema(readJson('schema', 'filter.schema.json'));
 const schema = readJson('schema', 'config.schema.json');
 const validate = ajv.compile(schema);
@@ -27,17 +27,14 @@ if (!validate(data)) {
 
 // eslint-disable-next-line no-console
 console.info('starting with the env:', env);
-nconf.overrides(data);
+
+nconf.file({ file: `${BASE_PATH}config/${env}.json` });
 // eslint-disable-next-line no-console
 console.info('mongo host:', nconf.get('mongo:host'));
 // eslint-disable-next-line no-console
 console.info('database:', nconf.get('mongo:db'));
 
 nconf.set('mode:dev', env === 'development');
-nconf.set(
-  'db:url',
-  `mongodb+srv://${env === 'production' ? `${nconf.get('mongo:user')}:${nconf.get('mongo:password')}@` : ''}${nconf.get(
-    'mongo:host'
-  )}/${nconf.get('mongo:db')}?retryWrites=true&w=majority`
-);
+nconf.set('db:url', `mongodb://${nconf.get('mongo:host')}/${nconf.get('mongo:db')}?retryWrites=true`);
+
 module.exports = nconf;
